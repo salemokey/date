db697ad633d136c5e4ae6b0c15d7ba521c7430fc
salemokey <flipsh96@gmail.com>
2023-11-14 17:06:33 +0300

    Задача 1 выполнена

---
+++ scripts/js.js
@@ -0,0 +1,8 @@
+function task1 () {
+    let data = new Date();
+    let den= ["воскресенье","понедельник", "вторник", "среда","четверг", "пятница", "суббота"];
+    num=data.getDay();
+    day=den[num];
+    alert(day);
+}
+f
